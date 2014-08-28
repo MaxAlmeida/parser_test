e9ea542f1ce5b52b1948d272761b7c3e21d2e86c
MaxAlmeida <llewxam100@hotmail.com>
2014-08-28 00:42:13 -0300

    permitindo pegar multiplos arquivos

--- app/models/procon.rb
+++ app/models/procon.rb
@@ -5,7 +5,7 @@ def self.import(file)
     CSV.foreach(file.path, col_sep: ',', headers: true, encoding: "iso-8859-1:UTF-8") do |row|
       Procon.create! :DDD => row[7], :cargo => row[5],
       :email => row[12], :endereco => row[6], :fax => row[10], 
-      :responsavel => row[4], :telefone => row[8]
+      :responsavel => row[4], :telefone => [row[8] ,row[9]]
 
       #{:AnoAtendimento => row[0], :TrimestreAtendimento => row[1]}
     end
